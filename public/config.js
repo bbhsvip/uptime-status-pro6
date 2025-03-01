fad946b28a6de86e6b074aee6735429835dfedd5
bbhsvip <bbhsvip@hotmail.com>
2025-03-01 19:50:25 +0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '寒舍网络空间监控站',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2849306-50b49f2bf7f8b0ef737573c8',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 背景图片
+  Image: 'https://ps.ssl.qhmsg.com/t0200ce14e175af6dcf.jpg',
+
+  // 背景颜色
+  color: '#555555',
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://wwqq.us.kg'
+    },
+    {
+      text: 'Github',
+      url: 'https://github.com/bbhssvip/uptime-status-pro'
+    },
+  ],
+};
